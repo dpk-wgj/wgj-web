d954fffed2c98f265877a2e907247424bd293342
YannYao <849723885@qq.com>
2018-07-10 13:32:15 +0800

    catNumber -> carNumber

--- src/main/java/com/dpk/wgj/config/webSocket/WebSocketConfig.java
+++ src/main/java/com/dpk/wgj/config/webSocket/WebSocketConfig.java
@@ -1,22 +1,14 @@
 package com.dpk.wgj.config.webSocket;
 
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-/**
- * Created by zhoulin on 2018/7/9.
- * 说明:
- */
-public class WebSocketConfig implements WebSocketConfigurer {
+@Configuration
+public class WebSocketConfig {
     @Bean
-    public WebSocketHandler newTeacherHandler() {
-        return new TeacherWebSocket();
-    }
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(newTeacherHandler(),"/teacherwebsocket").setAllowedOrigins("*").addInterceptors(new HandShake());
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
