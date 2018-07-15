415f21228659c213749c27c67c96b3e89e338f89
ZERO <2817748290@qq.com>
2018-07-15 19:24:58 +0800

    用户日志行为多条件查询功能修复

--- src/main/java/com/dpk/wgj/bean/tableInfo/LogInfoMessage.java
+++ src/main/java/com/dpk/wgj/bean/tableInfo/LogInfoMessage.java
@@ -6,6 +6,26 @@ public class LogInfoMessage extends TableMessage {
 
     private LogInfo logInfo = new LogInfo();
 
+    private String startTime;
+
+    private String endTime;
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
     public LogInfo getLogInfo() {
         return logInfo;
     }
