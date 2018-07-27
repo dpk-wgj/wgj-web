5712069d553ffe4bd28e11416742ac0945d18156
hlx <hlx520you1314>
2018-07-27 14:49:01 +0800

    727

--- src/main/java/com/dpk/wgj/WgjApplication.java
+++ src/main/java/com/dpk/wgj/WgjApplication.java
@@ -2,10 +2,17 @@ package com.dpk.wgj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class WgjApplication {
+public class WgjApplication  {
+//    @Overrideextends SpringBootServletInitializer
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(WgjApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(WgjApplication.class, args);
